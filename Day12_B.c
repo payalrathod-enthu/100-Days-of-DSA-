37c080e2bd51550dd36cd4089a087fa1e53b710b
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2026-02-16 17:24:57 +0530

    Convert Toeplitz matrix check to main function
    Refactor the Toeplitz matrix check into a main function that takes user input for matrix dimensions and elements.

--- Day12_B.c
+++ Day12_B.c
@@ -1,17 +1,38 @@
-class Solution {
-public:
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+#include <stdio.h>
 
-        // Start from (1,1) because first row/column have no top-left neighbor
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] != matrix[i-1][j-1]) {
-                    return false;
-                }
+int main() {
+    int m, n;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int isToeplitz = 1;  // assume true
+
+    // Check diagonals
+    for(int i = 1; i < m; i++) {
+        for(int j = 1; j < n; j++) {
+            if(matrix[i][j] != matrix[i-1][j-1]) {
+                isToeplitz = 0;
+                break;
             }
         }
-        return true;
+        if(isToeplitz == 0)
+            break;
     }
-};
+
+    if(isToeplitz)
+        printf("True (Toeplitz Matrix)");
+    else
+        printf("False (Not a Toeplitz Matrix)");
+
+    return 0;
+}
+
