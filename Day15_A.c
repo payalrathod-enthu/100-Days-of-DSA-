b62f98ed1e88e9722417b5d536ea65582f3d0f85
payalrathod-enthu <payal.28512@stu.upes.ac.in>
2026-02-16 12:38:32 +0530

    Create Day15_A.c

---
+++ Day15_A.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    // Input matrix
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    // Calculate primary diagonal sum
+    for (int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    // Output result
+    printf("%d\n", sum);
+
+    return 0;
+}
